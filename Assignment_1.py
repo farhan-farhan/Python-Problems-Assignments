6fd26650262ba45dcadbb92a77e8a185ac22dd25
farhan <108405018+farhan-farhan@users.noreply.github.com>
2024-04-01 16:18:37 +0530

    Add files via upload

---
+++ Assignment_1.py
@@ -0,0 +1,33 @@
+# 1. Write a Python program to print the following string in a specific format (see the output).  
+# Output :  
+# Twinkle, twinkle, little star,
+# 	How I wonder what you are! 
+# 		Up above the world so high,   		
+# 		Like a diamond in the sky. 
+# Twinkle, twinkle, little star, 
+# 	How I wonder what you are
+print('''Twinkle, twinkle, little star,
+  	How I wonder what you are! 
+ 		 Up above the world so high,   		
+ 	 	Like a diamond in the sky. 
+  Twinkle, twinkle, little star, 50.  
+
+  	How I wonder what you are ''')
+# 2. WAP to print names of your 5 friends each in new line using single print() and single string.
+print("Amman\nfamed\nyavor\nsaifali\naskar")
+# 3. WAP to print names of your 5 friends each in new line using single print() and independent string for each name.
+print("Amman","\nfamed","\nyavor","\nsaifali","\naskar")
+
+# 4. WAP to print names of your 5 friends each in new line using 5 print().
+print("Amaan")
+print("fameed")
+print("yavor")
+print("saifali")
+print("askar")
+# 5. WAP to print \t and \n as it is in the output.
+print("\\n","\\t")
+
+
+
+
+
