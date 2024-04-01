aafc351ee90d79b52ba566037e579ab84fcf369b
farhan <108405018+farhan-farhan@users.noreply.github.com>
2024-04-01 16:19:38 +0530

    Add files via upload

---
+++ Assignment_4_pattern.py
@@ -0,0 +1,144 @@
+    
+# *
+# * *
+# * * *
+# * * * *
+# * * * * *
+# for i in range(1,6):
+#     for j in range(1,6):
+#          if j<=i:
+#              print("*",end=" ")
+#          else:
+#              print(" ",end=" ")   
+             
+#     print(" ")  
+ 
+ 
+ 
+#         *  
+#       * *  
+#     * * *  
+#   * * * *  
+# * * * * *   
+# for i in range(1,6):
+#     for j in range(1,6):
+#          if j>=6-i:
+#              print("*",end=" ")
+#          else:
+#              print(" ",end=" ")   
+             
+#     print(" ")      
+  
+       
+# * * * * *  
+#   * * * *
+#     * * *
+#       * *
+#         *
+# for i in range(1,6):
+#     for j in range(1,6):
+#          if j>=i:
+#              print("*",end=" ")
+#          else:
+#              print(" ",end=" ")   
+             
+#     print(" ") 
+
+
+
+# * * * * *  
+# * * * *
+# * * *
+# * *
+# *
+# for i in range(1,6):
+#     for j in range(1,6):
+#          if j<=6-i:
+#              print("*",end=" ")
+#          else:
+#              print(" ",end=" ")   
+             
+#     print(" ") 
+
+#         *
+#       * * *
+#     * * * * *      
+#   * * * * * * *
+# * * * * * * * * *  
+# for i in range(1,6):
+#     for j in range(1,10):
+#          if j>=6-i and j<=4+i:
+#              print("*",end=" ")
+#          else:
+#              print(" ",end=" ")   
+             
+#     print(" ") 
+
+# * * * * * * * * *  
+#   * * * * * * *
+#     * * * * *
+#       * * *
+#         *
+# for i in range(1,6):
+#     for j in range(1,10):
+#          if j>=i and j<=10-i:
+#              print("*",end=" ")
+#          else:
+#              print(" ",end=" ")   
+             
+#     print(" ")  
+# * * * * *
+#   * * * * *        
+#     * * * * *      
+#       * * * * *    
+#         * * * * *  
+# for i in range(1,6):
+#     for j in range(1,10):
+#          if j>=i and j<=4+i:
+#              print("*",end=" ")
+#          else:
+#              print(" ",end=" ")   
+             
+#     print(" ")  
+
+#         *
+#       * * *
+#     * * * * *      
+#   * * * * * * *
+# * * * * * * * * *
+# * * * * * * * * *  
+#   * * * * * * *
+#     * * * * *
+#       * * *
+#         *
+# for i in range(1,6):
+#     for j in range(1,10):
+#          if j>=6-i and j<=4+i:
+#              print("*",end=" ")
+#          else:
+#              print(" ",end=" ")   
+             
+#     print(" ")  
+# for i in range(1,6):
+#     for j in range(1,10):
+#          if j>=i and j<=10-i:
+#              print("*",end=" ")
+#          else:
+#              print(" ",end=" ")   
+             
+#     print(" ") 
+
+# k=0
+# for i in range(1,6):
+#     k+=1 if i<5 else k-1
+#     # i<5?k=k+1 :k-1
+#     for j in range(1,10):
+#          if j>=6-k and j<=4+k:
+#              print("*",end=" ")
+#          else:
+#              print(" ",end=" ")   
+             
+#     print(" ")      
+
+
+
